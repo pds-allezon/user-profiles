2027eb34db076fb37e3f62f753d25411241f33e6
questras <michal.wisniewski422@gmail.com>
2022-08-16 17:21:28 +0200

    Project Setup.

---
+++ src/test/java/pl/mwisniewski/userprofiles/UserProfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mwisniewski.userprofiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
