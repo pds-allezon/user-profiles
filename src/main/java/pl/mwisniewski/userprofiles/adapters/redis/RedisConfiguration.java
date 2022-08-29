87554e89fd08d4d478ad7f0edf8fd9005be1262e
questras <michal.wisniewski422@gmail.com>
2022-08-29 19:54:08 +0200

    Update redis config.

--- src/main/java/pl/mwisniewski/userprofiles/adapters/redis/RedisConfiguration.java
+++ src/main/java/pl/mwisniewski/userprofiles/adapters/redis/RedisConfiguration.java
@@ -2,6 +2,7 @@ package pl.mwisniewski.userprofiles.adapters.redis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -9,7 +10,11 @@ import org.springframework.data.redis.core.RedisTemplate;
 public class RedisConfiguration {
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(
+                "st133vm108.rtb-lab.pl", 6666
+        );
+
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
