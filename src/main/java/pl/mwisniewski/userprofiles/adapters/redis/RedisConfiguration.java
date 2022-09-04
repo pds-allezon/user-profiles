76edc1fa26efc5861ec44b3c319c8c69b9a83746
questras <michal.wisniewski422@gmail.com>
2022-09-04 10:38:54 +0200

    Fix bean.

--- src/main/java/pl/mwisniewski/userprofiles/adapters/redis/RedisConfiguration.java
+++ src/main/java/pl/mwisniewski/userprofiles/adapters/redis/RedisConfiguration.java
@@ -3,6 +3,7 @@ package pl.mwisniewski.userprofiles.adapters.redis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -25,6 +26,7 @@ public class RedisConfiguration {
     }
 
     @Bean
+    @Primary
     public RedisTemplate<String, String> redisTemplate() {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
